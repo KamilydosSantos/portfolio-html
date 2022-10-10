2b49fd73e239ba30a00868aa708da5b59cb27ec5
KamilydosSantos <kamily7803@gmail.com>
2022-10-10 20:04:16 -0300

    feat: page width and links positions

--- js/skills-hover.js
+++ js/skills-hover.js
@@ -1,44 +1,43 @@
-
 // habilidades
 
-const skillsItems = document.querySelectorAll('.skills-items li');
-const skillsDescriptions = document.querySelectorAll('.skills-descriptions li');
+const skillsItems = document.querySelectorAll(".skills-items li");
+const skillsDescriptions = document.querySelectorAll(".skills-descriptions li");
 
 function activeSkill(index) {
-    skillsDescriptions.forEach(item => {
-        item.classList.remove('active');
-    });
-    skillsDescriptions[index].classList.add('active');
-    skillsItems.forEach(item => {
-        item.classList.remove('focus');
-    });
-    skillsItems[index].classList.add('focus');
+  skillsDescriptions.forEach((item) => {
+    item.classList.remove("active");
+  });
+  skillsDescriptions[index].classList.add("active");
+  skillsItems.forEach((item) => {
+    item.classList.remove("focus");
+  });
+  skillsItems[index].classList.add("focus");
 }
 
 skillsItems.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeSkill(index);
-    });
+  itemMenu.addEventListener("click", () => {
+    activeSkill(index);
+  });
 });
 
 // header scroll
 
-const introduction = document.getElementById('introduction');
-const skills = document.getElementById('skills');
-const projects = document.getElementById('projects');
+const introduction = document.getElementById("introduction");
+const skills = document.getElementById("skills");
+const projects = document.getElementById("projects");
 
 function goIntroduction() {
-    window.scroll(0, 0)
+  window.scroll(0, 0);
 }
 
 function goSkills() {
-    window.scroll(0, 800)
+  window.scroll(0, 880);
 }
 
 function goProjects() {
-    window.scroll(0, 1600);
+  window.scroll(0, 1600);
 }
 
-introduction.addEventListener('click', goIntroduction);
-skills.addEventListener('click', goSkills);
-projects.addEventListener('click', goProjects);
+introduction.addEventListener("click", goIntroduction);
+skills.addEventListener("click", goSkills);
+projects.addEventListener("click", goProjects);
